2741f8bb9c455a46300111380e28dc919e2691e9
Daria <dashasemenyuta@gmail.com>
2018-03-20 20:13:46 +0200

    Initial commit

---
+++ src/project/App.java
@@ -0,0 +1,8 @@
+package project;
+
+public class App {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/project/services/impl/ContactServiceImpl.java
@@ -0,0 +1,6 @@
+package project.services.impl;
+
+import project.services.ContctService;
+
+public class ContactServiceImpl implements ContctService {
+}
